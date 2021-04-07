c704d93333b631587930b039eca1cc4b9c6eb38d
TiaZheng <tia.zheng22@sitechhs.com>
2021-04-06 21:11:09 -0400

    done?!!

--- index.js
+++ index.js
@@ -77,34 +77,37 @@ const titles = [
 //Array of authors and the book they wrote
 //"--- wrote --- in ---"
 const wroteIn = books.map( (books) =>
-  books.authorFirst + " " + books.authorLast + ` wrote ` + `"` + books.name + `"` + ` in ` + books.publishDate
-);
+  books.authorFirst + " " + books.authorLast + ` wrote ` + `"` + books.name + `"` + ` in ` + books.publishDate);
 console.log(wroteIn);
 
 
-
-
 //Sort books from oldest to most recent
+const yearOrder = books.sort((a, b)=>{
+  if(a.publishDate > b.publishDate){
+    return 1;
+  }
+  else{
+    return -1;
+  }
+});
+console.log(yearOrder);
 
-
-/*var bookDate = [];
-for (let i = 0; i < books.length; i++) {
-  bookDate[i] = books[i].publishDate;
-}
-bookDate.sort(function(a, b){return a-b});
-console.log(bookDate);*/
 
 //sort books alphabetically
-
 const order = titles.sort();
 console.log(order);
 
 
 //Find who wrote War and Peace
+const WAP = books.find(names =>{
+if (names.name === "War and Peace") {
+  return true;
+}
+});
+console.log(WAP.authorFirst + ` ` + WAP.authorLast);
 
 
 //how many books were written before 1900? 
-
   const prior = books.filter(books =>{
   if (books.publishDate < 1900) {
     return true;
@@ -113,26 +116,19 @@ console.log(order);
 console.log(prior.length);
 
 
-
 //was there at least one book published within the last 100 years?
-/*const within = books.filter(function(books){
-  if (books.publishDate > 2021)
-})*/
+const within = books.some(books =>{
+  if (books.publishDate > 1921) {
+    return true;
+  }
+});
+console.log(within);
+
+
 //was every book published within the last 100 years?
-
-const sentence = books.map(
-  (book) =>
-    book.authorFirst +
-    " " +
-    book.authorLast +
-    " " +
-    "wrote" +
-    " " +
-    book.name +
-    " " +
-    "in" +
-    " " +
-    book.publishDate
-);
-console.log(sentence);
-
+const every = books.every(books =>{
+  if(books.publishDate >= 1921) {
+    return true;
+  }
+});
+console.log(every);
